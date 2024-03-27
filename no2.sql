ad65949cef64ad446641c4dd31032725b3e49349
tanatornkab <tanatornkab2019@gmail.com>
2024-03-28 00:11:46 +0700

    Add files via upload

---
+++ no2.sql
@@ -0,0 +1,42 @@
+create table #CardScan (
+	EmployeeID int,
+  Clock datetime2
+)
+create table #WorkSchedule (
+	EmployeeID int,
+  WorkDate date,
+  BeginTime datetime2,
+  EndTime datetime2
+)
+
+
+INSERT INTO #CardScan (EmployeeID, Clock)
+VALUES (1, '2012-01-01 07:00:00'),
+       (1, '2012-01-01 12:00:00'),
+       (1, '2012-01-01 17:00:00'),
+       (2, '2012-01-02 08:40:00'),
+       (2, '2012-01-02 20:00:00');
+
+INSERT INTO #WorkSchedule (EmployeeID, WorkDate, BeginTime, EndTime)
+VALUES (1, '2012-01-01', '2012-01-01 08:00:00', '2012-01-01 17:00:00'),
+       (1, '2012-01-02', '2012-01-02 08:00:00', '2012-01-02 17:00:00'),
+       (2, '2012-01-01', '2012-01-01 10:00:00', '2012-01-01 20:00:00'),
+       (2, '2012-01-02', '2012-01-02 10:00:00', '2012-01-02 20:00:00');
+
+
+
+select * from #CardScan
+
+select 
+    t1.EmployeeID,
+    t1.WorkDate,
+    min(t2.Clock) as ClockIn,
+    max(t2.Clock) as ClockOut
+ from #WorkSchedule t1 
+inner join #CardScan t2 on t1.EmployeeID = t2.EmployeeID 
+    and CAST(Clock as Date) = WorkDate
+    Group by t1.EmployeeID,
+    t1.WorkDate
+
+drop table #CardScan
+drop table #WorkSchedule
